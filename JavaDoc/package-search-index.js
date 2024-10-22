742f733b055e2a5b5110155071906cdadab7c4bb
Jaroslav Kopča <95101788+jaroslavkopca@users.noreply.github.com>
2024-10-22 11:52:36 +0200

    Delete JavaDoc directory

--- JavaDoc/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"cz.cvut.fel.pjv"},{"l":"cz.cvut.fel.pjv.control"},{"l":"cz.cvut.fel.pjv.data"},{"l":"cz.cvut.fel.pjv.map"},{"l":"cz.cvut.fel.pjv.OpenSky"}];updateSearchResults();
